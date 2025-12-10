7ed7e648f37e753418245fdb381ac87f6b5e3c15
Hikaru Kusanagi <hikarugp24@gmail.com>
2025-12-10 12:00:00 +0900

    Initial commit

--- lib/page/navigatiton.dart
+++ lib/page/navigatiton.dart
@@ -13,108 +13,104 @@ class _NavigationPageState extends State<NavigationPage> {
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      endDrawer: NavigationDrawer( // 右側のナビゲーションドロワー
-        children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-            child: Text(
-              'Navigation Drawer',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
+    return DefaultTabController(
+      length: 3, // タブの数
+      initialIndex: 1, // 最初に表示するタブのインデックス
+      child: Scaffold(
+        endDrawer: NavigationDrawer( // 右側のナビゲーションドロワー
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Navigation Drawer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
               ),
             ),
-          ),
-          // NavigationRailの実装の仕方が分からないため、一旦コメントアウトします。
-          // NavigationRail(destinations: const [
-          //   NavigationRailDestination(
-          //     icon: Icon(Icons.home),
-          //     label: Text('Home'),
-          //   ),
-          //   NavigationRailDestination(
-          //     icon: Icon(Icons.settings),
-          //     label: Text('Settings'),
-          //   ),
-          // ], selectedIndex: currentPageIndex, onDestinationSelected: (int index) {
-          //   setState(() {
-          //     currentPageIndex = index;
-          //   });
-          // },),
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Home'),
-            onTap: () {
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Settings'),
-            onTap: () {
-            },
-          ),
-        ],
-      ), // 右側のナビゲーションドロワー
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ), // ← これがないと自動で戻るボタンが出ない
-        title: Text('Navigation Page'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.info),
-            onPressed: () {
-              // Action when info button is pressed
-            },
-          ),
-        ], // 右側のアクションボタン
-        flexibleSpace: Container( // AppBarの背景をカスタマイズ
-          decoration: BoxDecoration( // グラデーション背景
-            gradient: LinearGradient(
-              colors: [Colors.purple, Colors.deepPurpleAccent],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+            // NavigationRailの実装の仕方が分からないため、一旦コメントアウトします。
+            // NavigationRail(destinations: const [
+            //   NavigationRailDestination(
+            //     icon: Icon(Icons.home),
+            //     label: Text('Home'),
+            //   ),
+            //   NavigationRailDestination(
+            //     icon: Icon(Icons.settings),
+            //     label: Text('Settings'),
+            //   ),
+            // ], selectedIndex: currentPageIndex, onDestinationSelected: (int index) {
+            //   setState(() {
+            //     currentPageIndex = index;
+            //   });
+            // },),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+              },
             ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+              },
+            ),
+          ],
+        ), // 右側のナビゲーションドロワー
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ), // ← これがないと自動で戻るボタンが出ない
+          title: Text('Navigation Page'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.info),
+              onPressed: () {
+                // Action when info button is pressed
+              },
+            ),
+          ], // 右側のアクションボタン
+          bottom: TabBar(
+            indicatorColor: Colors.amber, // インジケーターの色を設定
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.cloud_outlined)),
+              Tab(icon: Icon(Icons.beach_access_sharp)),
+              Tab(icon: Icon(Icons.brightness_5_sharp)),
+            ],
           ),
-        ), // グラデーション背景
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(20.0), // 高さを指定
-          child: Container(
-            color: Colors.blueAccent,
-            height: 4.0,
-          ),
-        ), // 下部のカスタムウィジェット
-      ),
-      body: Center(
-        child: Text('Navigation Page'),
-      ),
-       bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index; // 選択されたインデックスを更新
-          });
-        },
-      indicatorColor: Colors.amber, // インジケーターの色を設定
-      selectedIndex: currentPageIndex, // 選択されたインデックスを設定
-      destinations: [
-          NavigationDestination(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
+        ),
+        body: Center(
+          child: Text('Navigation Page'),
+        ),
+         bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index; // 選択されたインデックスを更新
+            });
+          },
+        indicatorColor: Colors.amber, // インジケーターの色を設定
+        selectedIndex: currentPageIndex, // 選択されたインデックスを設定
+        destinations: [
+            NavigationDestination(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+        ),
       ),
     );
   }
